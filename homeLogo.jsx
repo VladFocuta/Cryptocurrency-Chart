75c501213b89c0c06bbed6c56d0f2cf5f15bdcad
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-08-15 12:16:31 +0200

    homeLogo.jsx

---
+++ homeLogo.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { LogoIcon } from '../images/logo';
+
+class HomeLogo extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <div className='homeLogo' > <LogoIcon />
+        </div>
+      </div>
+    )
+  }
+}
+export default HomeLogo
