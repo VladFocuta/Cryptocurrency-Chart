c138d453321ee1e8374cc8acab9815b61ea45955
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-08-15 12:16:52 +0200

    navBar.jsx

---
+++ navBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  state = {}
+  render() {
+    return (
+      <div className="p-3 mb-2 bg-info bg-gradient fw-medium">
+        <p className="fs-2"> Crypto  World </p>
+      </div>
+    )
+  }
+}
+export default NavBar
